870a36e71e9e14c4e8e27964a3cb2f37c9f9f3bd
alik1223 <43026754+alik1223@users.noreply.github.com>
2020-06-14 23:10:20 +0300

    change yadro.h

---
+++ Yadro2.h
@@ -0,0 +1,36 @@
+#ifndef Yadro_h
+#define Yadro_h
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+class Yadro
+{
+private:
+	float x, y, dx, dy;
+	float V0, alpha;
+	bool life = true;
+	Yadro(Texture &texture);
+	static Yadro *s;
+public:
+	static Yadro *getinstance(Texture &texture);//static function
+	Sprite sprite;
+	void set_x(float x);
+	void set_y(float y);
+	void set_dx(float dx);
+	void set_dy(float dy);
+	void set_V0(float V0);
+	void set_alpha(float alpha);
+	void set_life(bool life);
+	float get_x();
+	float get_y();
+	float get_dx();
+	float get_dy();
+	float get_V0();
+	float get_alpha();
+	bool get_life();
+	FloatRect getRect();
+	Sprite get_sprite();
+
+};
+#endif
